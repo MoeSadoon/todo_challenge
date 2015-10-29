479aa156bf52c376aa414f28025a02bf66b91419
MoeSadoon <msadoon590@gmail.com>
2015-10-29 17:44:22 +0000

    button active on bootstrap works

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ var app = angular.module('toDo',[ ]);
 
 app.controller('toDoCtrl', function($scope) {
 
+  $scope.tab = 1;
+
   $scope.todos = [
     'first',
     'second',
     'third',
   ];
+
+  $scope.completed =[
+    'completed task'
+
+  ];
+
+  $scope.isSet = function(newValue) {
+    return $scope.tab === newValue
+  };
 
   $scope.done = function(todo) {
     var indexOf = $scope.todos.indexOf(todo);
